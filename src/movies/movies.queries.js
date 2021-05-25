ccbb7ec54249d7572aa770fbcb17ded82e34cca0
imjae <code@kakao.com>
2021-05-26 05:01:15 +0900

    파일 구조 변경 (src추가)

---
+++ src/movies/movies.queries.js
@@ -0,0 +1,10 @@
+import client from "../client";
+
+export default {
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => {
+      return client.movie.findUnique({ where: { id } });
+    },
+  },
+};
