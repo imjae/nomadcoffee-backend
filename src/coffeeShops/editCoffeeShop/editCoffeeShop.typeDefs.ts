c2722fea56769561a7e2b86d5d479ee4f412a682
imjae <code@kakao.com>
2021-06-05 21:11:58 +0900

    build, deploy

--- src/coffeeShops/editCoffeeShop/editCoffeeShop.typeDefs.ts
+++ src/coffeeShops/editCoffeeShop/editCoffeeShop.typeDefs.ts
@@ -7,6 +7,13 @@ export default gql`
   }
 
   type Mutation {
-    editCoffeeShop(id: Int!, name: String, latitude: String, longitude: String, categories: [String], photos: [String]): MutationResult
+    editCoffeeShop(
+      id: Int!
+      name: String
+      latitude: String
+      longitude: String
+      categories: [String]
+      photos: [String]
+    ): MutationResult
   }
 `;
